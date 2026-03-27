1b587a9e9e2ff0fef7a26b6a6dc8adeeac86143a
Haruto Suzuki <szkharuto0407@gmail.com>
2026-03-27 18:31:34 -0400

    Changed dataset to buterfly

--- Classification/DenseNet/dataset.py
+++ Classification/DenseNet/dataset.py
@@ -13,7 +13,7 @@ def download_data(data_dir):
     data_dir = Path(BASE_PATH) / data_dir
     data_dir.mkdir(parents=True, exist_ok=True)
 
-    download_path = kagglehub.dataset_download("sshikamaru/fruit-recognition")
+    download_path = kagglehub.dataset_download("gpiosenka/butterfly-images40-species")
 
     print("Path to dataset files:", download_path)
 
@@ -23,10 +23,10 @@ def download_data(data_dir):
     print("Moving complete!")
     return data_dir
 
-def build_transforms(image_size=100, train=True):
+def build_transforms(image_size=224, train=True):
     if train:
         return T.Compose([
-            T.RandomResizedCrop(image_size, scale=(0.8, 1.0)),
+            T.RandomResizedCrop(image_size),
             T.RandomHorizontalFlip(),
             T.RandomRotation(15),
             T.ToTensor(),
@@ -42,25 +42,22 @@ def build_transforms(image_size=100, train=True):
 class ButterflyDataset(Dataset):
     def __init__(self, root, split="train", transform=None):
         self.root = BASE_PATH / Path(root)
-        self.train_dir = self.root / "train/train"
-        self.test_dir = self.root / "test/test"
+        self.data_dir = self.root / split
         self.transform = transform
 
-        self.classes = sorted([d.name for d in self.train_dir.iterdir() if d.is_dir()])
-        self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(self.classes)}
+        self.df = pd.read_csv(self.root / "butterflies and moths.csv")
+
+        data = self.df[self.df["data set"] == split]
+        _unique_id_and_classes = data[["class id", "labels"]].drop_duplicates().reset_index(drop=True)
+        self.class_to_idx = {cls_name: id for id, cls_name in _unique_id_and_classes.itertuples(index=False)}
 
         self.samples = []
-        if split == "train":
-            for cls_name in self.train_dir.iterdir():
-                cls_folder = self.train_dir / cls_name
-                for img_path in cls_folder.iterdir():
-                    if img_path.suffix.lower() in [".jpg", ".jpeg", ".png"]:
-                        self.samples.append((img_path, self.class_to_idx[cls_name.name]))
-        else:
-            self.df = pd.read_csv(self.root / "sampleSubmission.csv")
-            for _, row in self.df.iterrows():
-                img_path = self.test_dir / f"{int(row['id']):04d}.jpg"
-                self.samples.append((img_path, self.class_to_idx[row['label']]))
+        for _, row in data.iterrows():
+            filename = row["filepaths"]
+            label_name = row["labels"]
+            label_idx = self.class_to_idx[label_name]
+            filepath = self.root / filename
+            self.samples.append((filepath, label_idx))
 
     def __len__(self):
         return len(self.samples)
